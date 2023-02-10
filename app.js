74c958ca40e5c168b7793e924c0ac2f6cf4d6db0
sridev01 <117353390+sridev01@users.noreply.github.com>
2023-02-10 15:06:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+const canvas = document.querySelector(".canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.height;
+
+const context = canvas.getContext("2d");
+const frameCount = 120;
+
+const currentFrame = (index) => './Renders/${(index + 1).toString()}.png';
+const images = [];
+let ball = { frame: 0 };
+
+for (let i = 0; i < frameCount; i++) {
+  const img = new Image();
+  img.src = currentFrame(i);
+  images.push(img);
+}
+
+
+gsap.to(ball, {
+  frame: frameCount - 1,
+  snap: 'frame',
+  ease: 'none',
+  scrollTrigger: {
+    scrub: true,
+    pin: "canvas",
+    end: "500%",
+    markers: true,
+  },
+  onUpadate: render,
+});
+
+
+images[0].onload = render;
+
+function render() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(images[ball.frame], 0, 0);
+}
